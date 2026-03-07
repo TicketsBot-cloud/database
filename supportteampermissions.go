414d4a1c9565192f12949a657e60c00d83f65290
biast12 <tobiaspoulsen@hotmail.com>
2026-03-07 23:19:37 +0100

    Add external emoji and sticker permissions
    Introduce two new boolean permissions (UseExternalEmojis, UseExternalStickers) to SupportTeamPermissions. Update defaultSupportTeamPermissions to enable them by default, add corresponding columns to the support_team_permissions table (with default true), and update SQL queries and Exec/Scan calls in Get, Set, and GetForTeams to include these fields.

--- supportteampermissions.go
+++ supportteampermissions.go
@@ -16,6 +16,8 @@ type SupportTeamPermissions struct {
 	SendTTSMessages        bool `json:"send_tts_messages"`
 	UseApplicationCommands bool `json:"use_application_commands"`
 	MentionEveryone        bool `json:"mention_everyone"`
+	UseExternalEmojis      bool `json:"use_external_emojis"`
+	UseExternalStickers    bool `json:"use_external_stickers"`
 }
 
 type SupportTeamPermissionsTable struct {
@@ -39,7 +41,9 @@ CREATE TABLE IF NOT EXISTS support_team_permissions(
 	"send_voice_messages"     bool NOT NULL DEFAULT 't',
 	"send_tts_messages"       bool NOT NULL DEFAULT 't',
 	"use_application_commands" bool NOT NULL DEFAULT 't',
-	"mention_everyone"        bool NOT NULL DEFAULT 'f',
+	"mention_everyone"         bool NOT NULL DEFAULT 'f',
+	"use_external_emojis"      bool NOT NULL DEFAULT 't',
+	"use_external_stickers"    bool NOT NULL DEFAULT 't',
 	FOREIGN KEY("team_id") REFERENCES support_team("id") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("team_id")
 );
@@ -56,12 +60,14 @@ func defaultSupportTeamPermissions() SupportTeamPermissions {
 		SendTTSMessages:        true,
 		UseApplicationCommands: true,
 		MentionEveryone:        false,
+		UseExternalEmojis:      true,
+		UseExternalStickers:    true,
 	}
 }
 
 func (c *SupportTeamPermissionsTable) Get(ctx context.Context, teamId int) (SupportTeamPermissions, error) {
 	query := `
-SELECT "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone"
+SELECT "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone", "use_external_emojis", "use_external_stickers"
 FROM support_team_permissions
 WHERE "team_id" = $1;`
 
@@ -75,6 +81,8 @@ WHERE "team_id" = $1;`
 		&perms.SendTTSMessages,
 		&perms.UseApplicationCommands,
 		&perms.MentionEveryone,
+		&perms.UseExternalEmojis,
+		&perms.UseExternalStickers,
 	)
 
 	if err != nil {
@@ -89,11 +97,11 @@ WHERE "team_id" = $1;`
 
 func (c *SupportTeamPermissionsTable) Set(ctx context.Context, teamId int, perms SupportTeamPermissions) error {
 	query := `
-INSERT INTO support_team_permissions("team_id", "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone")
-VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-ON CONFLICT("team_id") DO UPDATE SET "send_messages" = $2, "embed_links" = $3, "attach_files" = $4, "add_reactions" = $5, "send_voice_messages" = $6, "send_tts_messages" = $7, "use_application_commands" = $8, "mention_everyone" = $9;`
+INSERT INTO support_team_permissions("team_id", "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone", "use_external_emojis", "use_external_stickers")
+VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+ON CONFLICT("team_id") DO UPDATE SET "send_messages" = $2, "embed_links" = $3, "attach_files" = $4, "add_reactions" = $5, "send_voice_messages" = $6, "send_tts_messages" = $7, "use_application_commands" = $8, "mention_everyone" = $9, "use_external_emojis" = $10, "use_external_stickers" = $11;`
 
-	_, err := c.Exec(ctx, query, teamId, perms.SendMessages, perms.EmbedLinks, perms.AttachFiles, perms.AddReactions, perms.SendVoiceMessages, perms.SendTTSMessages, perms.UseApplicationCommands, perms.MentionEveryone)
+	_, err := c.Exec(ctx, query, teamId, perms.SendMessages, perms.EmbedLinks, perms.AttachFiles, perms.AddReactions, perms.SendVoiceMessages, perms.SendTTSMessages, perms.UseApplicationCommands, perms.MentionEveryone, perms.UseExternalEmojis, perms.UseExternalStickers)
 	return err
 }
 
@@ -105,7 +113,7 @@ func (c *SupportTeamPermissionsTable) GetForTeams(ctx context.Context, teamIds [
 	}
 
 	query := `
-SELECT "team_id", "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone"
+SELECT "team_id", "send_messages", "embed_links", "attach_files", "add_reactions", "send_voice_messages", "send_tts_messages", "use_application_commands", "mention_everyone", "use_external_emojis", "use_external_stickers"
 FROM support_team_permissions
 WHERE "team_id" = ANY($1);`
 
@@ -118,7 +126,7 @@ WHERE "team_id" = ANY($1);`
 	for rows.Next() {
 		var teamId int
 		var perms SupportTeamPermissions
-		if err := rows.Scan(&teamId, &perms.SendMessages, &perms.EmbedLinks, &perms.AttachFiles, &perms.AddReactions, &perms.SendVoiceMessages, &perms.SendTTSMessages, &perms.UseApplicationCommands, &perms.MentionEveryone); err != nil {
+		if err := rows.Scan(&teamId, &perms.SendMessages, &perms.EmbedLinks, &perms.AttachFiles, &perms.AddReactions, &perms.SendVoiceMessages, &perms.SendTTSMessages, &perms.UseApplicationCommands, &perms.MentionEveryone, &perms.UseExternalEmojis, &perms.UseExternalStickers); err != nil {
 			return nil, err
 		}
 		result[teamId] = perms
