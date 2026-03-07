3e7e4b35d27cd2981c0d1cb61136ed6dd6ad2ca6
biast12 <tobiaspoulsen@hotmail.com>
2026-03-07 17:04:50 +0100

    RM-44: Add more ticket permission
    Introduce additional ticket permission flags and wire them through the schema and code. Added tp_* columns to the panels table and corresponding TicketPerms* fields on the Panel struct; updated all relevant SELECT/INSERT/UPDATE queries and field pointer mapping. Expanded TicketPermissions to include send_voice_messages, send_tts_messages, use_external_emojis, and use_external_stickers, updated the ticket_permissions table schema, and adjusted Get/Set queries (defaults set to true). Also updated multipaneltargets select to include the new panel permission columns.

--- ticketpermissions.go
+++ ticketpermissions.go
@@ -7,9 +7,13 @@ import (
 )
 
 type TicketPermissions struct {
-	AttachFiles  bool `json:"attach_files"`
-	EmbedLinks   bool `json:"embed_links"`
-	AddReactions bool `json:"add_reactions"`
+	AttachFiles         bool `json:"attach_files"`
+	EmbedLinks          bool `json:"embed_links"`
+	AddReactions        bool `json:"add_reactions"`
+	SendVoiceMessages   bool `json:"send_voice_messages"`
+	SendTTSMessages     bool `json:"send_tts_messages"`
+	UseExternalEmojis   bool `json:"use_external_emojis"`
+	UseExternalStickers bool `json:"use_external_stickers"`
 }
 
 type TicketPermissionsTable struct {
@@ -29,6 +33,10 @@ CREATE TABLE IF NOT EXISTS ticket_permissions(
 	"attach_files" bool NOT NULL DEFAULT 't',
 	"embed_links" bool NOT NULL DEFAULT 't',
 	"add_reactions" bool NOT NULL DEFAULT 't',
+	"send_voice_messages" bool NOT NULL DEFAULT 't',
+	"send_tts_messages" bool NOT NULL DEFAULT 't',
+	"use_external_emojis" bool NOT NULL DEFAULT 't',
+	"use_external_stickers" bool NOT NULL DEFAULT 't',
 	PRIMARY KEY("guild_id")
 );
 `
@@ -36,7 +44,7 @@ CREATE TABLE IF NOT EXISTS ticket_permissions(
 
 func (c *TicketPermissionsTable) Get(ctx context.Context, guildId uint64) (TicketPermissions, error) {
 	query := `
-SELECT "attach_files", "embed_links", "add_reactions"
+SELECT "attach_files", "embed_links", "add_reactions", "send_voice_messages", "send_tts_messages", "use_external_emojis", "use_external_stickers"
 FROM ticket_permissions
 WHERE "guild_id" = $1;`
 
@@ -45,14 +53,22 @@ WHERE "guild_id" = $1;`
 		&permissions.AttachFiles,
 		&permissions.EmbedLinks,
 		&permissions.AddReactions,
+		&permissions.SendVoiceMessages,
+		&permissions.SendTTSMessages,
+		&permissions.UseExternalEmojis,
+		&permissions.UseExternalStickers,
 	)
 
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return TicketPermissions{
-				AttachFiles:  true,
-				EmbedLinks:   true,
-				AddReactions: true,
+				AttachFiles:         true,
+				EmbedLinks:          true,
+				AddReactions:        true,
+				SendVoiceMessages:   true,
+				SendTTSMessages:     true,
+				UseExternalEmojis:   true,
+				UseExternalStickers: true,
 			}, nil
 		} else {
 			return TicketPermissions{}, err
@@ -64,11 +80,11 @@ WHERE "guild_id" = $1;`
 
 func (c *TicketPermissionsTable) Set(ctx context.Context, guildId uint64, permissions TicketPermissions) (err error) {
 	query := `
-INSERT INTO ticket_permissions("guild_id", "attach_files", "embed_links", "add_reactions")
-VALUES($1, $2, $3, $4)
-ON CONFLICT("guild_id") DO UPDATE SET "attach_files" = $2, "embed_links" = $3, "add_reactions" = $4;`
+INSERT INTO ticket_permissions("guild_id", "attach_files", "embed_links", "add_reactions", "send_voice_messages", "send_tts_messages", "use_external_emojis", "use_external_stickers")
+VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT("guild_id") DO UPDATE SET "attach_files" = $2, "embed_links" = $3, "add_reactions" = $4, "send_voice_messages" = $5, "send_tts_messages" = $6, "use_external_emojis" = $7, "use_external_stickers" = $8;`
 
-	_, err = c.Exec(ctx, query, guildId, permissions.AttachFiles, permissions.EmbedLinks, permissions.AddReactions)
+	_, err = c.Exec(ctx, query, guildId, permissions.AttachFiles, permissions.EmbedLinks, permissions.AddReactions, permissions.SendVoiceMessages, permissions.SendTTSMessages, permissions.UseExternalEmojis, permissions.UseExternalStickers)
 	return
 }
 
